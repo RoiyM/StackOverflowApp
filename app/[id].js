9692f38eefe83aa0e2e56767471790f784fb94df
roiy <roiy355@gmail.com>
2023-06-05 18:39:41 +0300

    Answers FlatList

--- app/[id].js
+++ app/[id].js
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
 import { useSearchParams } from "expo-router";
 import questions from "../data/questions.json";
 import QuestionHeader from "../src/components/QuestionHeader";
+import answers from "../data/answers.json";
+import AnswerListItem from "../src/components/AnswerListItem";
 
 export default function details() {
   const { id } = useSearchParams();
@@ -10,9 +12,18 @@ export default function details() {
   if (!question) {
     return <text>Question Not Found!</text>;
   }
+  // return (
+  //   <View style={{ backgroundColor: "white", flex: 1 }}>
+  //     <QuestionHeader question={question} />
+  //   </View>
+  // );
   return (
     <View style={{ backgroundColor: "white", flex: 1 }}>
-      <QuestionHeader question={question} />
+      <FlatList
+        data={answers.items}
+        renderItem={({ item }) => <AnswerListItem answer={item} />}
+        ListHeaderComponent={() => <QuestionHeader question={question} />}
+      />
     </View>
   );
 }
